4cfef84ca621cb46ffac0b3ead183ac01028a6a2
anbelespinal <anabelespinal>
2016-08-25 16:09:56 -0500

    ejerc cuatro

---
+++ main.js
@@ -0,0 +1,7 @@
+var numero1=prompt("tiempo de llamada");
+var costollamada=0.20;
+
+var llamada=parseInt(numero1)*costollamada;
+document.write("tu costo es: s/. ");
+document.write(llamada);
+document.write(" por" + numero1 +" minutos")
